397e4e4c1f8a3603a6ddf809b5ab27944b0c64ee
Tessio94 <simeklapan@gmail.com>
2025-06-04 08:59:52 +0200

    added supabase connection string

--- server/database.js
+++ server/database.js
@@ -2,16 +2,25 @@ const pg = require("pg");
 const { Pool } = pg;
 require("dotenv").config();
 
-const password = process.env.SIMEPSSW;
+// const password = process.env.SIMEPSSW;
+
+// const pool = new Pool({
+// 	host: "localhost",
+// 	user: "postgres",
+// 	database: "perntodo",
+// 	password: password,
+// 	max: 20,
+// 	idleTimeoutMillis: 30000,
+// 	connectionTimeoutMillis: 2000,
+// });
+
+const password = process.env.DATABASE_URL_PROD;
 
 const pool = new Pool({
-	host: "localhost",
-	user: "postgres",
-	database: "simetodo",
-	password: password,
-	max: 20,
-	idleTimeoutMillis: 30000,
-	connectionTimeoutMillis: 2000,
+	connectionString: password,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = pool;
