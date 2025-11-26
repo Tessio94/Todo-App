aedf44e007cca5a75d8eb0e3f0e82b60f81297f6
Tessio94 <simeklapan@gmail.com>
2025-11-26 14:11:53 +0100

    changed api's to correct url

--- client/src/components/Task.tsx
+++ client/src/components/Task.tsx
@@ -4,90 +4,90 @@ import Icon from "../components/Icon";
 import { useTodos } from "../context";
 
 type TaskProps = {
-  index: number;
-  id: number;
-  description: string;
-  done: boolean;
-  type: "add" | string;
+	index: number;
+	id: number;
+	description: string;
+	done: boolean;
+	type: "add" | string;
 };
 
 const Task = ({ index, id, description, done, type }: TaskProps) => {
-  const [check, setCheck] = useState<boolean>(done);
-  const { updateTodo, deleteTodo } = useTodos();
+	const [check, setCheck] = useState<boolean>(done);
+	const { updateTodo, deleteTodo } = useTodos();
 
-  async function handleCheck() {
-    try {
-      const updatedCheck = !check;
-      setCheck((check) => !check);
+	async function handleCheck() {
+		try {
+			const updatedCheck = !check;
+			setCheck((check) => !check);
 
-      const response = await fetch("https://todo-app-as64.onrender.com/", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id,
-          done: updatedCheck,
-        }),
-      });
-      const data = await response.json();
-      // console.log(data);
-      // console.log(`done: ${done}`);
-      updateTodo(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			const response = await fetch("https://api.app1.tessio94.com/", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					id,
+					done: updatedCheck,
+				}),
+			});
+			const data = await response.json();
+			// console.log(data);
+			// console.log(`done: ${done}`);
+			updateTodo(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async function handleDelete() {
-    try {
-      const response = await fetch("https://todo-app-as64.onrender.com/", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          id,
-        }),
-      });
-      const data = await response.json();
-      console.log(data);
-      // console.log(`done: ${done}`);
-      deleteTodo(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async function handleDelete() {
+		try {
+			const response = await fetch("https://api.app1.tessio94.com/", {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					id,
+				}),
+			});
+			const data = await response.json();
+			console.log(data);
+			// console.log(`done: ${done}`);
+			deleteTodo(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  // console.log(`Done: ${done}`);
+	// console.log(`Done: ${done}`);
 
-  return (
-    <div className="max-w-full w-full flex  items-center justify-between gap-5 border-purple-600 border-2 rounded-xl py-1 px-3 bg-slate-200 shadow-lg hover:shadow-xl hover:bg-slate-300 transition-all duration-300">
-      <p
-        className={`text-purple-600 text-xl flex items-end gap-5 overflow-auto py-1 ${
-          check ? "line-through" : ""
-        }`}
-      >
-        <span className="text-orange-600 text-2xl font-bold">{index + 1}.</span>
-        {description}
-      </p>
-      {type === "add" ? (
-        <div className="flex items-center gap-5">
-          <Button type="checkbox" onClick={handleCheck}>
-            <Icon iconType="check" checked={check} />
-          </Button>
+	return (
+		<div className="max-w-full w-full flex  items-center justify-between gap-5 border-purple-600 border-2 rounded-xl py-1 px-3 bg-slate-200 shadow-lg hover:shadow-xl hover:bg-slate-300 transition-all duration-300">
+			<p
+				className={`text-purple-600 text-xl flex items-end gap-5 overflow-auto py-1 ${
+					check ? "line-through" : ""
+				}`}
+			>
+				<span className="text-orange-600 text-2xl font-bold">{index + 1}.</span>
+				{description}
+			</p>
+			{type === "add" ? (
+				<div className="flex items-center gap-5">
+					<Button type="checkbox" onClick={handleCheck}>
+						<Icon iconType="check" checked={check} />
+					</Button>
 
-          <Button type="link" href={`/edit/${id}`}>
-            <Icon iconType="edit" />
-          </Button>
+					<Button type="link" href={`/edit/${id}`}>
+						<Icon iconType="edit" />
+					</Button>
 
-          <Button type="button" onClick={handleDelete}>
-            <Icon iconType="delete" />
-          </Button>
-        </div>
-      ) : null}
-    </div>
-  );
+					<Button type="button" onClick={handleDelete}>
+						<Icon iconType="delete" />
+					</Button>
+				</div>
+			) : null}
+		</div>
+	);
 };
 
 export default Task;
--- client/src/context.tsx
+++ client/src/context.tsx
@@ -1,63 +1,63 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
 type Todo = {
-  todo_id: number;
-  description: string;
-  done: boolean;
+	todo_id: number;
+	description: string;
+	done: boolean;
 };
 
 type TodosContextType = {
-  todos: Todo[];
-  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-  updateTodo: (updatedTodo: Todo) => void;
-  deleteTodo: (deletedTodo: Todo) => void;
+	todos: Todo[];
+	setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+	updateTodo: (updatedTodo: Todo) => void;
+	deleteTodo: (deletedTodo: Todo) => void;
 };
 
 const TodosContext = createContext<TodosContextType | undefined>(undefined);
 
 export const TodosProvider = ({ children }: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-  useEffect(() => {
-    async function getAllTodos() {
-      try {
-        const response = await fetch("https://todo-app-as64.onrender.com/");
-        if (!response.ok) throw new Error("Failed to fetch todos");
+	useEffect(() => {
+		async function getAllTodos() {
+			try {
+				const response = await fetch("https://api.app1.tessio94.com/");
+				if (!response.ok) throw new Error("Failed to fetch todos");
 
-        const data = await response.json();
-        setTodos(data);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    }
-    getAllTodos();
-  }, []);
+				const data = await response.json();
+				setTodos(data);
+			} catch (error) {
+				console.error("Error fetching todos:", error);
+			}
+		}
+		getAllTodos();
+	}, []);
 
-  const updateTodo = (updatedTodo: Todo) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.todo_id === updatedTodo.todo_id ? updatedTodo : todo
-      )
-    );
-  };
+	const updateTodo = (updatedTodo: Todo) => {
+		setTodos((prevTodos) =>
+			prevTodos.map((todo) =>
+				todo.todo_id === updatedTodo.todo_id ? updatedTodo : todo
+			)
+		);
+	};
 
-  const deleteTodo = (deletedTodo: Todo) => {
-    setTodos((prevTodos) =>
-      prevTodos.filter((todo) => todo.todo_id !== deletedTodo.todo_id)
-    );
-  };
+	const deleteTodo = (deletedTodo: Todo) => {
+		setTodos((prevTodos) =>
+			prevTodos.filter((todo) => todo.todo_id !== deletedTodo.todo_id)
+		);
+	};
 
-  return (
-    <TodosContext.Provider value={{ todos, setTodos, updateTodo, deleteTodo }}>
-      {children}
-    </TodosContext.Provider>
-  );
+	return (
+		<TodosContext.Provider value={{ todos, setTodos, updateTodo, deleteTodo }}>
+			{children}
+		</TodosContext.Provider>
+	);
 };
 
 export const useTodos = () => {
-  const context = useContext(TodosContext);
-  if (!context) {
-    throw new Error("useTodos must be used within a TodosProvider");
-  }
-  return context;
+	const context = useContext(TodosContext);
+	if (!context) {
+		throw new Error("useTodos must be used within a TodosProvider");
+	}
+	return context;
 };
