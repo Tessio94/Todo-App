6f6f23eb26152141c5c53dd49ee6dccf129707e8
Tessio94 <simeklapan@gmail.com>
2025-11-26 12:59:58 +0100

    changed backend port

--- server/database.js
+++ server/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const password = process.env.SIMEPSSW;
 
 const pool = new Pool({
-	host: "localhost",
+	host: "db",
 	user: "postgres",
 	database: "perntodo",
 	password: password,
